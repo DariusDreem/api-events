0acd27127a56f33af1390236965d72222e0bbf8f
DariusDreem <reungoat.nathan@gmail.com>
2026-03-06 10:52:25 +0100

    fix url

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`✅ API Events démarrée sur http://localhost:${port}`);
+    console.log(`✅ API Events démarrée sur http://localhost:${port}/events`);
 });
